4dab1cd90e02fc18d79f3c7efc4abc103f375603
shivachetkuri <88610376+shivachetkuri@users.noreply.github.com>
2021-08-13 17:25:35 +0530

    created conitional statements food items
    food items and price displayed using conditional statements

---
+++ conditionalstatements:fooditems.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+printf("pick an food item:\n1.Pizza, Rs 239\n2.Burger, Rs 129\n3.Pasta, Rs 179\n4.French Fries, Rs 99\n5.Sandwich, Rs 149\n6.exit\n");
+int choice;
+scanf("%d",&choice);
+switch (choice) {
+            case 1:
+                printf("Food item - Pizza\nPrice - Rs 239");
+                break;
+            case 2:
+                printf("Food item - Burger\nPrice - Rs 129");
+                break;
+            case 3:
+                printf("Food item - Pasta\nPrice - Rs 179");
+                break;
+            case 4:
+                printf("Food item - French Fries\nPrice - Rs 99");
+                break;
+            case 5:
+                printf("Food item - Sandwich\nPrice - Rs 149");
+                break;
+            default:
+                printf("INVALID!");
+        }
+        return 0;
+}
